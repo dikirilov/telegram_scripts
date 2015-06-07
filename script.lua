c671bbe6713e2909c255548be2a36d87406f1782
Дмитрий Кириловский <dikirilovskiy@gmai.com>
2015-06-07 22:37:23 +0800

    First commit: utf8 is realized, JSON treatment is performed, getting currency from JSON is performed

---
+++ script.lua
@@ -0,0 +1,40 @@
+  local utf8 = require 'utf8'
+  local http = require("socket.http")
+  local json = (loadfile "/home/bananapi/tg/scripts/JSON.lua")()
+  function on_msg_receive (msg)
+      if msg.out then
+          return
+      end
+--      print(utf8.lower(msg.text))
+      if (utf8.find(utf8.lower(msg.text),'курс')) then
+	 local data = http.request "http://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDRUB,EURRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+	 local cur = json:decode(data)
+	 local res = ""
+	 local wtf = "Курс "
+	 if (utf8.find(utf8.lower(msg.text),'евро'))then
+	  wtf = wtf .. "евро: "
+	  res = cur.query.results.rate[2]
+	 else
+	  wtf = wtf .. "доллара: "
+	  res = cur.query.results.rate[1]
+	 end
+--	 print(res)
+--	 for k,v in pairs(res) do print(k,v) end
+	 send_msg (msg.from.print_name, wtf .. tostring(res.Rate), ok_cb, false)
+      end
+  end
+   
+  function on_our_id (id)
+  end
+   
+  function on_secret_chat_created (peer)
+  end
+   
+  function on_user_update (user)
+  end
+   
+  function on_chat_update (user)
+  end
+   
+  function on_get_difference_end ()
+  end
